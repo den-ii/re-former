725623a21150c5cc43de5cb6e8a175ef37d77a8e
den-ii <wisdomochiche@gmail.com>
2024-06-14 19:01:21 +0100

    complete create a user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,19 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
 
+    if @user.save
+      redirect_to new_user_path, notice: "User created"
+    else
+      render :new, status: :unprocessable_entity
+    end
+
+  end
+
+  private
+
+  def user_params
+   params.require(:user).permit(:name, :email, :password)
   end
 end
