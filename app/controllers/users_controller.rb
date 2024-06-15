c546916f5aa9d377ecd7751be3d1899b6c2a80e7
den-ii <wisdomochiche@gmail.com>
2024-06-15 12:09:50 +0100

    edit done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,9 +17,26 @@ class UsersController < ApplicationController
 
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+   @user = User.find(params[:id])
+   puts "Userrerrrrrrrrrrrr"
+
+
+   if @user.update(user_params)
+    redirect_to new_user_path, notice: "User updated"
+  else
+    render :edit, status: :unprocessable_entity
+  end
+
+  end
+
   private
 
-  # def user_params
-  #  params.require().permit(:name, :email, :password)
-  # end
+  def user_params
+   params.require(:user).permit(:name, :email, :password)
+  end
 end
